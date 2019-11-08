0e3d8f8ce789b19d07a5431f1c02bb389ab59c8c
mmatagne <57266870+mmatagne@users.noreply.github.com>
2019-11-08 18:46:02 +0100

    Update testpsq.cpp

--- testpsq.cpp
+++ testpsq.cpp
@@ -1,16 +1,37 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
+#include <string>
+
 using namespace std;
 
 int main() {
+	ifstream protFile ("P00533.fasta");
+	if (!protFile) 
+	{
+		cout << "Error" << endl;
+	}
+	string firstLine;
+	getline(protFile,firstLine);
+	char ch;
+	while(protFile >> noskipws >> ch) 
+	{
+		if(ch != '\n') 
+		{
+			cout << ch;
+		}
+	}
     int8_t i1; //Les int8_t sont des int codés sur 8 bits, donc 1 byte (sizeof(i1) = 1).
     std::ifstream myFile ("P00533.fasta.psq", std::ios::in | std::ios::binary);
     if (!myFile) {
         std::cout << "Error";
     }
-    else {
-        while(myFile.read((char*) (&i1), sizeof(i1))) {
-	    std::cout << (int) i1 << std::endl; //Convertir le int8_t en int est obligatoire pour l'affichage
+    myFile.read((char*) (&i1), sizeof(i1));
+    while(myFile.read((char*) (&i1), sizeof(i1))) {
+		if((int) i1 == 0) {
+			//cout << "New Protein" << endl;
+			break;
+		}
+		//std::cout << (int) i1 << std::endl; //Convertir le int8_t en int est obligatoire pour l'affichage
 	}
-    }
 }
