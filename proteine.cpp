0833c8c14fef1573004701b99ec494319a64493d
mmatagne <mmatagne@ulb.ac.be>
2019-12-08 23:19:33 +0100

    Mise a jour 8/12, il faut encore la blosum et la structure des meilleures protéines

--- proteine.cpp
+++ proteine.cpp
@@ -3,42 +3,21 @@
 using std::cout;
 using std::endl;
 
-Proteine::Proteine(string new_name = "Unknown protein", vector<int> new_data = {}) : name(new_name), data(new_data) {
-	//cout << "Nouvelle proteine de nom : " << name << endl;
+Proteine::Proteine(int new_protNb, forward_list<int> new_listeInconnue, forward_list<int> new_listeDb) {
+	protNb = new_protNb;
+	listeInconnue = new_listeInconnue;
+	listeDb = new_listeDb;
 }
 Proteine::~Proteine(){
 	
 }
 
-void Proteine::setName(string new_name){
-	name = new_name;
+
+int Proteine::getNb() const{
+	return protNb;
 }
 
-void Proteine::setData(vector<int> new_data){
-	data = new_data;
-}
-
-string Proteine::getName() const{
-	return name;
-}
-
-vector<int> Proteine::getData() const{
-	return data;
-}
-
-int Proteine::getLength() const{
-	return data.size();
-}
-
-bool Proteine::operator==(const Proteine &protDroite) const{
-	return data==protDroite.getData();
-}
-
-
-/* fonction main pour tester l'egalite de deux proteines (deux proteines sont egales si elles ont les memes chaines d'acides amines) 
-int main(){
+void Proteine::printResult() {
 	
-	Proteine prot1("Proteine 1", {2 ,5 ,6} ), prot2("Proteine 2", {2, 5, 6});
-	bool var = prot1==prot2;
-	cout << var << endl;
-}*/ 
+}
+
