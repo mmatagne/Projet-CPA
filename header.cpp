2b25a311cf2618189f5b7d62a0e340b7317d74b7
Luderic_VAN_CALCK <ludericvancalck@gmail.com>
2019-12-12 18:14:34 +0100

    test

--- header.cpp
+++ header.cpp
@@ -10,55 +10,38 @@ Header::Header(string phrfile) {
 }
 
 Header::~Header(){
-	phr.close();
+	
 }
 
 char* Header::getTitle(int offset) {
 	phr.seekg(offset);
-	uint8_t var;
+	int8_t var;
 	while(phr.read( (char*)(&var), sizeof(var)))
 	{
-		if(var == 160) //A0 en hex
+		if((unsigned int)(unsigned char) var == 160) //A0 en hex
 		{
-			phr.read( (char*)(&var), sizeof(var));
-			if(var == 128) //80
+			int8_t var2;
+			phr.read( (char*)(&var2), sizeof(var2));
+			if((unsigned int)(unsigned char) var2 == 128) //80
 			{
-				phr.read( (char*)(&var), sizeof(var));
-				if(var == 26) //1A
+				int8_t var3;
+				phr.read( (char*)(&var3), sizeof(var3));
+				if((unsigned int)(unsigned char) var3 == 26) //1A
 				{
-					phr.read( (char*)(&var), sizeof(var));
-					
-					if(var >= 128) //1st bit is a 1 : var(-128 to set 1st bit to 0) = number of bytes on which the length of the title is encoded
+					int8_t var4;
+					phr.read( (char*)(&var4), sizeof(var4));
+					if((unsigned int)(unsigned char) var4 > 128)
 					{
-						int nbBytes = var-128;
-						unsigned long title_length ;
-						uint8_t x;
-						title_length = 0;
-						for (int j = 0; j < nbBytes; j++){ 
-							phr.read((char*)&x, 1);
-							title_length = title_length << 8 | x; //left shift de 8 cases et OR p/r à la variable x venant d'etre lue
-						}
-						
-						title = new char[title_length+1];
-						title[title_length] = '\0';
-						phr.read(title, title_length*sizeof(char));
-						cout << title_length << " : " << title << endl;
-						return title;
+						break;
 					}
-					else // 1st bit is a 0 : var = title_length
-					{
-						title = new char[var+1];
-						title[var] = '\0';
-						phr.read(title, var*sizeof(char));
-						cout<< title << endl;
-						return title;
+					else {
+						title2 = new char[var4];
+						phr.read(title2, var4);
+						return title2;
 					}
 				}
 			}
 		}
 	}
 }
-
-
-
 
